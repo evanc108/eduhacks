5a7e646ae1a60f2845570cbda244e01f2d3a69e8
Eric Tao <etao9039@gmail.com>
2024-01-05 15:47:50 -0800

    recording

--- eduhacks/src/App.js
+++ eduhacks/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AudioReactRecorder, { RecordState } from 'audio-react-recorder'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      recordState: null
+    }
+  }
+
+  start = () => {
+    this.setState({
+      recordState: RecordState.START
+    })
+  }
+
+  stop = () => {
+    this.setState({
+      recordState: RecordState.STOP
+    })
+  }
+
+  //audioData contains blob and blobUrl
+  onStop = (audioData) => {
+    console.log('audioData', audioData)
+  }
+
+  render() {
+    const { recordState } = this.state
+
+    return (
+      <div>
+        <AudioReactRecorder state={recordState} onStop={this.onStop} />
+
+        <button onClick={this.start}>Start</button>
+        <button onClick={this.stop}>Stop</button>
+      </div>
+    )
+  }
 }
 
 export default App;
